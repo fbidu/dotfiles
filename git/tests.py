c5cffeb1ef914b555779ef7ed8ea233aa274e0e9
Felipe Bidu Rodrigues <felipe@felipevr.com>
2021-05-03 18:57:45 -0300

    Update git tests

--- git/tests.py
+++ git/tests.py
@@ -1,5 +1,7 @@
 from pathlib import Path
-from dotfiles import checkers
+from uuid import uuid4
+import uuid
+from dotfiles import checkers, shell
 
 
 def test_git_installed():
@@ -7,6 +9,25 @@ def test_git_installed():
 
 
 def test_gitconfig_symlinked():
-    assert checkers.symlink(
-        Path("~/.gitconfig"), Path("~/dotfiles/git/gitconfig")
+    assert checkers.symlink(Path("~/.gitconfig"), Path("~/dotfiles/git/gitconfig"))
+
+
+def test_global_gitignore():
+    test_folder = Path("/") / "tmp" / str(uuid4())
+    included_file = str(uuid4())
+    shell.runsh(
+        f"""
+        mkdir {test_folder} &&\
+        cd {test_folder} \
+        git init &&\
+        mkdir env &&\
+        touch {included_file}
+    """
     )
+
+    output = shell.runsh(
+        f"cd {test_folder} && git status", capture_output=True, encoding="utf-8"
+    )
+
+    assert included_file in output.stdout
+    assert not "env" in output.stdout
