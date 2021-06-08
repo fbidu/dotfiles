3d64d038b70ef9cc12ce5b06237caac9a2baaf35
Felipe Bidu Rodrigues <felipe@felipevr.com>
2021-06-08 10:44:47 -0300

    Add secret handling

---
+++ zsh/secrets_template.zsh
@@ -0,0 +1,4 @@
+# You can add your secret environment variables such as Github tokens inside a secrets.zsh file.
+# It will be add by the zshrc bootstraper but will be ignored by git.
+
+export MY_SECRET_TOKEN=muahahahahaha
