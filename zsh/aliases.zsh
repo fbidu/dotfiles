a9373d33feb54284aa7b94b087c9331c706e699b
Felipe Bidu Rodrigues <felipe@felipevr.com>
2022-08-06 10:10:20 -0300

    add grepy alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,4 @@
 alias ls="ls -lh --group-directories-first --color=auto"
 alias cam="pkill -f gphoto2; gphoto2 --stdout --capture-movie | ffmpeg -hwaccel nvdec -c:v mjpeg_cuvid -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0"
 alias blmod="git status --porcelain | awk '{print \$\2}' | xargs black"
+function grepy () {grep "$1" -rn "${2-.}" --include="*.py" --color=always}
